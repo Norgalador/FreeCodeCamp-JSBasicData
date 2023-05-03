9306386d7e4787395d094063c414d53fa9e5dbd2
dojo-JoshBurlile <josh.burlile@gmail.com>
2023-05-03 14:14:25 -0700

    added addUsingSplice

---
+++ addUsingSplice.js
@@ -0,0 +1,11 @@
+// We have defined a function, htmlColorNames, which takes an array of HTML colors as an argument. Modify the function using splice() to remove the first two elements of the array and add 'DarkSalmon' and 'BlanchedAlmond' in their respective places.
+
+function htmlColorNames(arr) {
+    // Only change code below this line
+    const amountToDelete = 2;
+    arr.splice(0, amountToDelete,'DarkSalmon', 'BlanchedAlmond');
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
